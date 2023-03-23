b4dcdc48822af64142275140e8df814e6052c137
Lubern5 <99304345+Lubern5@users.noreply.github.com>
2023-03-23 11:02:37 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,12 +15,12 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "launch-wizard-36" 
 }
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
+    default     = "Phone 
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
